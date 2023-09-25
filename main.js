06a14f7ae8d945019be05547ee24daca650acc86
erikvella <vella.erik1@gmail.com>
2023-09-25 14:58:45 +0100

    [fix] milestone 2

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+// 1 Creare un array contenente tutti i post da stampare
+
+// 2 creare un ciclo per generarli autonomamente
+
+// 3 stampo in pagina
+
+// prendo le mie constanti/variabili
+
+const feed = document.querySelector('#container') ;
+
+
+
+// array con tutti i post
+
+
 const posts = [
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/300?image=171",
-        "author": {
+        "author":
+        {
             "name": "Phil Mangione",
             "image": "https://unsplash.it/300/300?image=15"
         },
@@ -14,7 +30,8 @@ const posts = [
         "id": 2,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=112",
-        "author": {
+        "author": 
+        {
             "name": "Sofia Perlari",
             "image": "https://unsplash.it/300/300?image=10"
         },
@@ -25,7 +42,8 @@ const posts = [
         "id": 3,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=234",
-        "author": {
+        "author": 
+        {
             "name": "Chiara Passaro",
             "image": "https://unsplash.it/300/300?image=20"
         },
@@ -36,7 +54,8 @@ const posts = [
         "id": 4,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=24",
-        "author": {
+        "author": 
+        {
             "name": "Luca Formicola",
             "image": null
         },
@@ -47,7 +66,8 @@ const posts = [
         "id": 5,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "media": "https://unsplash.it/600/400?image=534",
-        "author": {
+        "author": 
+        {
             "name": "Alessandro Sainato",
             "image": "https://unsplash.it/300/300?image=29"
         },
@@ -55,3 +75,44 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+
+// ciclo for each
+
+for(let post of posts){
+  
+    console.log(posts);
+
+  
+  container.innerHTML += ` <div class="post">
+  <div class="post__header">
+      <div class="post-meta">                    
+          <div class="post-meta__icon">
+              <img class="profile-pic" src=${post.author.image} alt="Phil Mangione">                    
+          </div>
+          <div class="post-meta__data">
+              <div class="post-meta__author">${post.author.name}</div>
+              <div class="post-meta__time">${post.created}</div>
+          </div>                    
+      </div>
+  </div>
+  <div class="post__text">${post.content}</div>
+  <div class="post__image">
+      <img src=${post.media} alt="">
+  </div>
+  <div class="post__footer">
+      <div class="likes js-likes">
+          <div class="likes__cta">
+              <a class="like-button  js-like-button" href="#" data-postid=${post.id}>
+                  <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                  <span class="like-button__label">Mi Piace</span>
+              </a>
+          </div>
+          <div class="likes__counter">
+              Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+          </div>
+      </div> 
+  </div>            
+</div>`
+}
